c6a024c4a1834107c8f08c574263599ed0564744
Aidan Pearson <apearson3@hawk.illinoistech.edu>
2026-07-05 21:15:16 +0000

    added some minors and sharps

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ let number = -1;
 let guess = "";
 let noteNumber = -1;
 let answer = "";
+let intervalNumber = -1;
 
 const notes = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
-const semitones = [0, 2, 4, 5, 7, 9, 11];
+const intervals = ["1", "2", "3", "b3", "4", "5", "b5", "#5", "6", "b6", "7", "b7", "8", "9", "b9", "10", "11", "#11", "13", "b13", "#13"]
+const semitones = [0, 2, 4, 3, 5, 7, 6, 8, 9, 8, 11, 10, 0, 2, 1, 4, 5, 6, 9, 8, 10];
 
 noteButton.addEventListener("click", genrateNote);
 intervalButton.addEventListener("click", genrateInterval);
@@ -36,28 +38,22 @@ function genrateNote() {
 function genrateInterval() {
     answerText.textContent = "";
     correctionText.textContent = "that is __";
-    number = (Math.floor(Math.random() * 12) + 1);
-    if (number == 12) {
-        number = 13;
-    }
-
-    intervalText.textContent = "the current interval is: " + number;
-    if (number > 7) {
-        number -= 7;
-    }
+    intervalNumber = (Math.floor(Math.random() * 21) + 1);
+    
+    intervalText.textContent = "the current interval is: " + intervals[intervalNumber];
 }
 
 function checkAnswer() {
     if (noteNumber == -1) {
         return;
     }
-    if (number == -1) {
+    if (intervalNumber == -1) {
         return;
     }
 
     guess = inputText.value;
 
-    answer = notes[(noteNumber + semitones[number-1]) % 12];
+    answer = notes[(noteNumber + semitones[intervalNumber]) % 12];
 
     if (answer.length > 1) {
         if (answer.substring(0, 2).toLowerCase() == guess.toLowerCase()) {
@@ -76,7 +72,7 @@ function checkAnswer() {
 
 function reset() {
     let note = "";
-    let number = -1;
+    let intervalNumber = -1;
     let guess = "";
     let noteNumber = -1;
     let answer = "";
